655610b380d86e996297972a13d3a1ec606e8991
waylanpunch <waylanpunch@gmail.com>
2016-06-07 07:45:17 +0800

    me again

--- app/src/main/java/com/way/heard/ui/fragments/ContactFragment.java
+++ app/src/main/java/com/way/heard/ui/fragments/ContactFragment.java
@@ -3,57 +3,38 @@ package com.way.heard.ui.fragments;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentPagerAdapter;
-import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.avos.avoscloud.AVUser;
 import com.way.heard.R;
-import com.way.heard.adapters.ContactTabAdapter;
-
-import java.util.ArrayList;
-import java.util.List;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class ContactFragment extends Fragment{
-    private final static String TAG = ContactFragment.class.getName();
+    //private final static String TAG = ContactFragment.class.getName();
 
     public static final String CONTACT = "Contact";
-
-
-    private TabLayout tabTile;                            //定义TabLayout
-    private ViewPager vpPager;                             //定义viewPager
-    private FragmentPagerAdapter fAdapter;                               //定义adapter
-
-    private List<Fragment> list_fragment;                                //定义要装fragment的列表
-    private List<String> list_title;                                     //tab名称列表
-
-    private FolloweeFragment followeeFragment;              //Followee Fragment
-    private FollowerFragment followerFragment;            //Follower Fragment
 
     public ContactFragment() {
         // Required empty public constructor
     }
 
-    public static ContactFragment newInstance(int param) {
-        ContactFragment contactFragment = new ContactFragment();
-        Bundle bundle = new Bundle();
-        bundle.putInt("ContactParam", param);
-        contactFragment.setArguments(bundle);
-        return contactFragment;
-    }
+//    public static ContactFragment newInstance(int param) {
+//        ContactFragment contactFragment = new ContactFragment();
+//        Bundle bundle = new Bundle();
+//        bundle.putInt("ContactParam", param);
+//        contactFragment.setArguments(bundle);
+//        return contactFragment;
+//    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        int param = getArguments().getInt("ContactParam");
+        //int param = getArguments().getInt("ContactParam");
     }
 
     @Override
@@ -66,36 +47,7 @@ public class ContactFragment extends Fragment{
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        this.tabTile = (TabLayout) view.findViewById(R.id.tab_contact_title);
-        this.vpPager = (ViewPager) view.findViewById(R.id.vp_contact_pager);
 
-        initData();
-    }
-
-    private void initData() {
-        followeeFragment = FolloweeFragment.newInstance(AVUser.getCurrentUser().getObjectId());
-        followerFragment = FollowerFragment.newInstance(AVUser.getCurrentUser().getObjectId());
-        //将fragment装进列表中
-        list_fragment = new ArrayList<>();
-        list_fragment.add(followeeFragment);
-        list_fragment.add(followerFragment);
-        list_title = new ArrayList<>();
-        list_title.add("Followee");
-        list_title.add("Follower");
-
-        //设置TabLayout的模式
-        tabTile.setTabMode(TabLayout.MODE_FIXED);
-        //为TabLayout添加tab名称
-        tabTile.addTab(tabTile.newTab().setText(list_title.get(0)));
-        tabTile.addTab(tabTile.newTab().setText(list_title.get(1)));
-
-        fAdapter = new ContactTabAdapter(getActivity().getSupportFragmentManager(), list_fragment, list_title);
-
-        //viewpager加载adapter
-        vpPager.setAdapter(fAdapter);
-        //tab_FindFragment_title.setViewPager(vp_FindFragment_pager);
-        //TabLayout加载viewpager
-        tabTile.setupWithViewPager(vpPager);
     }
 
 
--- app/src/main/java/com/way/heard/ui/fragments/MessageFragment.java
+++ app/src/main/java/com/way/heard/ui/fragments/MessageFragment.java
@@ -3,21 +3,33 @@ package com.way.heard.ui.fragments;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.way.heard.R;
+import com.way.heard.utils.LogUtil;
+
+import java.util.Arrays;
+import java.util.List;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class MessageFragment extends Fragment{
+public class MessageFragment extends Fragment {
     private final static String TAG = MessageFragment.class.getName();
 
     public static final String MESSAGE = "Message";
+    private ViewPager viewPager;
+    private TabLayout tabLayout;
+
 
     public MessageFragment() {
         // Required empty public constructor
@@ -47,6 +59,79 @@ public class MessageFragment extends Fragment{
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        viewPager = (ViewPager) view.findViewById(R.id.pager);
+        tabLayout = (TabLayout) view.findViewById(R.id.tablayout);
     }
 
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        initTabLayout();
+    }
+
+    private void initTabLayout() {
+        LogUtil.d(TAG, "initTabLayout debug");
+        try {
+            String[] tabList = new String[]{"会话", "联系人", "我"};
+            final Fragment[] fragmentList = new Fragment[]{new ConversationFragment(),
+                    new ContactFragment(), new PersonalProfileFragment()};
+
+            tabLayout.setTabMode(TabLayout.MODE_FIXED);
+            for (int i = 0; i < tabList.length; i++) {
+                tabLayout.addTab(tabLayout.newTab().setText(tabList[i]));
+            }
+
+            TabFragmentAdapter adapter = new TabFragmentAdapter(getChildFragmentManager(),
+                    Arrays.asList(fragmentList), Arrays.asList(tabList));
+            viewPager.setAdapter(adapter);
+            viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+                @Override
+                public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+                }
+
+                @Override
+                public void onPageSelected(int position) {
+                    if (0 == position) {
+                        //EventBus.getDefault().post(new ConversationFragmentUpdateEvent());
+                    }
+                }
+
+                @Override
+                public void onPageScrollStateChanged(int state) {
+                }
+            });
+            tabLayout.setupWithViewPager(viewPager);
+            tabLayout.setTabsFromPagerAdapter(adapter);
+        } catch (Exception e) {
+            LogUtil.e(TAG, "initTabLayout error", e);
+            Toast.makeText(getContext(), e.getMessage(), Toast.LENGTH_LONG).show();
+        }
+    }
+
+    public class TabFragmentAdapter extends FragmentStatePagerAdapter {
+
+        private List<Fragment> mFragments;
+        private List<String> mTitles;
+
+        public TabFragmentAdapter(FragmentManager fm, List<Fragment> fragments, List<String> titles) {
+            super(fm);
+            mFragments = fragments;
+            mTitles = titles;
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return mFragments.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return mFragments.size();
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return mTitles.get(position);
+        }
+    }
 }
